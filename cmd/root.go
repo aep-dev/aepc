3e4545f8f709887b5ad5555fa7b7b1c1d9521499
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2023-09-15 10:05:10 -0700

    fixing up work
    - applying licenses.
    - including output examples.

--- cmd/root.go
+++ cmd/root.go
@@ -1,3 +1,16 @@
+// Copyright 2023 Yusuke Fredrick Tsutsumi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package cmd
 
 import (
@@ -9,10 +22,10 @@ import (
 
 	"github.com/ghodss/yaml"
 
+	"github.com/aep-dev/aepc/reader"
+	"github.com/aep-dev/aepc/schema"
+	"github.com/aep-dev/aepc/writer/proto"
 	"github.com/spf13/cobra"
-	"github.com/toumorokoshi/aep-sandbox/aepc/reader"
-	"github.com/toumorokoshi/aep-sandbox/aepc/schema"
-	"github.com/toumorokoshi/aep-sandbox/aepc/writer/proto"
 	"google.golang.org/protobuf/encoding/protojson"
 )
 
