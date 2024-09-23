71f7d30513ba63ee309500834d0aa78b08bdef15
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2024-09-23 09:55:14 -0700

    fix(lint): use buf plugins for lint and fixes for AEP compliance. (#28)
    Using buf plugins for linting is a more ergonomic approach, avoiding the need to manually clone googleapis.
    
    api-linter was updated with fixes, so this change was just switching to buf lint and adding it to github actions: aep-dev/api-linter#93.

--- scripts/run-api-linter.sh
+++ scripts/run-api-linter.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-if ! which api-linter ; then
-    go install github.com/aep-dev/api-linter/cmd/api-linter@latest
+if ! which buf-plugin-aep > /dev/null; then
+    go install github.com/aep-dev/api-linter/cmd/buf-plugin-aep@latest
 fi
 
-if [ ! -d /tmp/googleapis ]; then
-    git clone https://github.com/googleapis/googleapis.git --depth=1 /tmp/googleapis
-fi
-
-api-linter \
-    ./example/bookstore/v1/bookstore.proto \
-    -I /tmp/googleapis \
-    --set-exit-status
+echo "Running API linter..."
+buf lint --path example/bookstore/v1/bookstore.proto
+echo "passed!"
