42d9e84931ea4173440b2da599085d4a3a4a67d5
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2025-08-19 22:02:57 -0700

    update script

--- scripts/copy-examples-to-aeps.sh
+++ scripts/copy-examples-to-aeps.sh
@@ -4,7 +4,7 @@
 set -ex
 
 SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
-AEPS_DIR="${SCRIPT_DIR}/../aeps"
+AEPS_DIR="${SCRIPT_DIR}/../../aeps"
 
-cp example/bookstore/v1/bookstore.yaml "${AEPS_DIR}/aeps/general/example.yaml"
-cp example/bookstore/v1/bookstore.proto "${AEPS_DIR}/aeps/general/example.proto"
+cp example/bookstore/v1/bookstore_openapi.yaml "${AEPS_DIR}/aep/general/example.oas.yaml"
+cp example/bookstore/v1/bookstore.proto "${AEPS_DIR}/aep/general/example.proto"
